59fca29c14406d84e11ce36501b9f8a873e7d269
Aisha-Moon <aishaaktermoon2@gmail.com>
2024-10-04 19:27:55 +0600

    user profile deatils showing and update

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::post('/send-otp', [UserController::class, 'sendOtpCode']);
 Route::post('/verify-otp', [UserController::class, 'verifyOtpCode']);
 Route::post('/reset-password', [UserController::class, 'ResetPass'])->middleware('tokenVerify');
 Route::get('/logout',[UserController::class,'UserLogout']);
+Route::post('/user-update',[UserController::class,'UpdateProfile'])->middleware('tokenVerify');
 
 
 
@@ -19,4 +20,9 @@ Route::get('/sendOtp',[UserController::class,'SendOtpPage']);
 Route::get('/verifyOtp',[UserController::class,'VerifyOTPPage']);
 Route::get('/resetPassword',[UserController::class,'ResetPasswordPage'])->middleware('tokenVerify');
 Route::get('/dashboard',[DashboardController::class,'DashboardPage'])->middleware('tokenVerify');
+Route::get('/user-profile',[UserController::class,'UserProfile'])->middleware('tokenVerify');
+Route::get('/userProfile',[UserController::class,'ProfilePage'])->middleware('tokenVerify');
+
+
+
 
